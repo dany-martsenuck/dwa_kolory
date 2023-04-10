75623d6654a0286f4acf08db24d985a155220c48
dany-martsenuck <62779649+dany-martsenuck@users.noreply.github.com>
2023-04-10 07:00:38 +0200

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,60 +1,128 @@
-const cartContainer = document.getElementById('cart-container');
-const cartTotal = document.getElementById('cart-total');
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
+document.addEventListener('DOMContentLoaded', () => {
+    displayCartItems();
+});
 
-function updateCart() {
-    cartContainer.innerHTML = '';
+function displayCartItems() {
+    const cartContainer = document.getElementById('cart-container');
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-    cart.forEach((item, index) => {
-        cartContainer.innerHTML += `
-            <div class="cart-item">
-                <img src="${item.image}" alt="${item.name}">
-                <div>
-                    <h3>${item.name}</h3>
-                    <p>${item.price} USD</p>
-                    <div class="quantity-wrapper">
-                        <button class="quantity-btn quantity-decrease" onclick="decreaseQuantity(${index})">-</button>
-                        <input type="number" class="quantity-input" value="${item.quantity}" min="1" onchange="updateQuantity(${index}, this)">
-                        <button class="quantity-btn quantity-increase" onclick="increaseQuantity(${index})">+</button>
-                    </div>
-                    <button class="delete-item-btn" onclick="deleteItem(${index})">Delete</button>
-                </div>
-            </div>
-        `;
-    });
-
-    let total = cart.reduce((acc, item) => acc + (item.price * item.quantity), 0);
-    cartTotal.innerHTML = `${total.toFixed(2)} USD`;
-
-    localStorage.setItem('cart', JSON.stringify(cart));
-}
-
-function updateQuantity(index, input) {
-    let newQuantity = parseInt(input.value);
-    if (isNaN(newQuantity) || newQuantity <= 0) {
-        input.value = cart[index].quantity;
+    if (cart.length === 0) {
+        cartContainer.innerHTML = '<p>Your cart is empty.</p>';
         return;
     }
 
-    cart[index].quantity = newQuantity;
-    updateCart();
+    cartContainer.innerHTML = '';
+
+    cart.forEach((item, index) => {
+        const cartItem = document.createElement('div');
+        cartItem.classList.add('cart-item');
+
+        const productImage = document.createElement('img');
+        productImage.src = item.imageUrl;
+        productImage.alt = item.productName;
+        productImage.classList.add('product-image');
+
+        const productNameElem = document.createElement('h2');
+        productNameElem.innerText = item.productName;
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = `$${item.price}`;
+
+        const quantityWrapper = document.createElement('div');
+        quantityWrapper.classList.add('quantity-wrapper');
+
+        const decreaseBtn = document.createElement('button');
+        decreaseBtn.innerText = '-';
+        decreaseBtn.classList.add('quantity-btn', 'quantity-decrease');
+        decreaseBtn.onclick = () => decreaseQuantity(index);
+
+        const quantityInput = document.createElement('input');
+        quantityInput.type = 'number';
+        quantityInput.value = item.quantity;
+        quantityInput.min = 1;
+        quantityInput.classList.add('quantity-input');
+        quantityInput.onchange = () => updateQuantity(index, quantityInput);
+
+        const increaseBtn = document.createElement('button');
+        increaseBtn.innerText = '+';
+        increaseBtn.classList.add('quantity-btn', 'quantity-increase');
+        increaseBtn.onclick = () => increaseQuantity(index);
+
+        quantityWrapper.appendChild(decreaseBtn);
+        quantityWrapper.appendChild(quantityInput);
+        quantityWrapper.appendChild(increaseBtn);
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'Delete';
+        deleteBtn.classList.add('delete-item-btn');
+        deleteBtn.onclick = () => deleteItem(index);
+
+        cartItem.appendChild(productImage);
+        cartItem.appendChild(productNameElem);
+        cartItem.appendChild(productPrice);
+        cartItem.appendChild(quantityWrapper);
+        cartItem.appendChild(deleteBtn);
+
+        cartContainer.appendChild(cartItem);
+    });
+
+    updateCartTotal();
 }
 
-function increaseQuantity(index) {
-    cart[index].quantity++;
-    updateCart();
+function updateCartTotal() {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    let total = 0;
+
+    cart.forEach(item => {
+        total += parseFloat(item.price) * item.quantity;
+    });
+
+    document.getElementById('cart-total-value').innerText = total.toFixed(2);
 }
 
 function decreaseQuantity(index) {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
     if (cart[index].quantity > 1) {
         cart[index].quantity--;
-        updateCart();
+        localStorage.setItem('cart', JSON.stringify(cart));
+        displayCartItems();
     }
 }
 
-function deleteItem(index) {
-    cart.splice(index, 1);
-    updateCart();
+function increaseQuantity(index) {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    cart[index].quantity++;
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCartItems();
 }
 
-updateCart();
+function updateQuantity(index, input) {
+    const newQuantity = parseInt(input.value);
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    if (newQuantity > 0) {
+        cart[index].quantity = newQuantity;
+    } else {
+        input.value = cart[index].quantity;
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCartItems();
+}
+
+function deleteItem(index) {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    cart.splice(index, 1);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    displayCartItems();
+}
+
+const checkoutBtn = document.getElementById('checkout-btn');
+checkoutBtn.addEventListener('click', () => {
+    alert('Thank you for your purchase!');
+    localStorage.removeItem('cart');
+    location.reload();
+});
